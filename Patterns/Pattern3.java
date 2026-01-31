2e1773d834d83fb9bac596af59b8cc6a47dfbbf0
Ashvani Sharma <panditashuabvp@gmail.com>
2026-01-31 23:06:37 +0530

    Pattern

---
+++ Patterns/Pattern3.java
@@ -0,0 +1,22 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern3 {
+    static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int row=1;
+        int star=n;
+        while (row<=n){
+            int i=1;
+            while (i<=star){
+                System.out.print(" * ");
+                i++;
+            }
+            row++;
+            star--;
+            System.out.println();
+        }
+    }
+}
