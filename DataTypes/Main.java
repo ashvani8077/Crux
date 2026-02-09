7ff477ed55c16bf8fd046c4aa59390010acd6833
Ashvani Sharma <panditashuabvp@gmail.com>
2026-02-09 18:42:00 +0530

    code

---
+++ DataTypes/Main.java
@@ -0,0 +1,14 @@
+package DataTypes;
+
+public class Main {
+    static void main(String[] args) {
+        byte a=127;
+        byte b=-128;
+        System.out.println(a);
+        System.out.println(b);
+        int d=129;
+        byte e=(byte)d;
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
